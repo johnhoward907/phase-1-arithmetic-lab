e3fde996bff2f143d99eec785ed1d6f6595276ca
John <jjhhoward1738@gmail.com>
2025-03-06 15:47:42 +0300

    adds file

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1=31
+let num2=2
+let multiply=num1*num2
+console.log(multiply)
+
+
+let random=parseInt(Math.random()*100) +1;
+console.log(random)
+
+
+let num3=16
+let num4=6
+let mod= num3%num4
+console.log(mod)
+
+let max=Math.max(16,18,10,20)
+console.log(max)
+
+
